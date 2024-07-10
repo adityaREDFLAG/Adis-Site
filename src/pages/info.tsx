8af0a76d86380f2cbb44576206cc9b09a2050010
Noblixzy <121813330+adityaREDFLAG@users.noreply.github.com>
2024-07-10 14:02:03 +0530

    Update info.tsx

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -6,10 +6,10 @@ import type { NextPage } from 'next'
 const ContentPage: NextPage = () => {
   return (
     <>
-      <NextSeo title="Info" />
+      <NextSeo title="Info" titleTemplate="%s"/>
       <div>
-        <h1>What's adi's studio</h1>
-        <p>idk</p>
+        <h1>What's adi's studio</h1> 
+        <p>Adi's studio is a upcoming studio! We are currently developing a bot and this site.</p>
       </div>
     </>
   )
