f6bb0f5800294775703c059c4e6475e5d79e002f
Noblixzy <121813330+adityaREDFLAG@users.noreply.github.com>
2024-07-10 12:47:21 +0530

    Update empty.tsx

--- src/pages/empty.tsx
+++ src/pages/empty.tsx
@@ -1,4 +1,7 @@
-import type { NextPage } from 'next'
+import * as React from 'react'
+ import { NextSeo } from 'next-seo'
+
+ import type { NextPage } from 'next'
 
  const EmptyPage: NextPage = () => {
    return <NextSeo title="Empty" />
