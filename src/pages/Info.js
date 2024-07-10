246a6c2e46b6a916ebda7b4918b8e6ac3f454401
Noblixzy <121813330+adityaREDFLAG@users.noreply.github.com>
2024-07-10 13:29:28 +0530

    Update Info.js

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -4,17 +4,19 @@ import { Box, Heading, Text } from '@chakra-ui/react';
 
 const InfoPage: React.FC = () => {
   return (
-    <>
+    <> {/* Opening JSX tag */}
       <NextSeo title="Info Page" />
       <Box p={4} maxW="container.lg">
         <Heading as="h2" size="lg">
           Welcome to the Information Page
         </Heading>
         <Text mt={4}>
-         Upcoming studio
+          This is a basic information page created using Next.js and Chakra UI.
+          You can replace this content with any information you want to display
+          on your website.
         </Text>
       </Box>
-    </>
+    </> {/* Closing JSX tag - Make sure it's after the last element */}
   );
 };
 
