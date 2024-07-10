aa8a3ebe71e12c638d10621768cfbf50c679f710
Noblixzy <121813330+adityaREDFLAG@users.noreply.github.com>
2024-07-10 12:40:48 +0530

    Update and rename info.tsx to empty.tsx

---
+++ src/pages/empty.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next'
+
+ const EmptyPage: NextPage = () => {
+   return <NextSeo title="Empty" />
+ }
+
+ export default EmptyPage
--- src/pages/info.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-import { NextSeo } from 'next-seo'
-
-import type { NextPage } from 'next'
-
-const EmptyPage: NextPage = () => {
-  return <NextSeo title="Info" />
-}
