e8888e43b3252b853083fcc373c100b285932794
Noblixzy <121813330+adityaREDFLAG@users.noreply.github.com>
2024-07-11 10:36:15 +0530

    Update info.tsx

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,27 +1,27 @@
-import * as React from 'react'
-import { NextSeo } from 'next-seo'
-import { Center, Box, Heading, Text } from '@chakra-ui/react'
-
-import type { NextPage } from 'next'
+import * as React from 'react';
+import { NextSeo } from 'next-seo';
+import { Center, Box, Heading, Text, Link, Button } from '@chakra-ui/react';
+import type { NextPage } from 'next';
 
 const ContentPage: NextPage = () => {
   return (
     <>
-      <NextSeo title="Info" titleTemplate="%s"/>
+      <NextSeo title="Info" titleTemplate="%s" />
       <Center minH="100vh">
         <Box textAlign="center" p={4}>
           <Heading as="h1" size="xl" mb={4}>
             What's Adi's Studio
           </Heading>
-          <Text fontSize="lg">
+          <Text fontSize="lg" mb={6}>
             Adi's studio is an upcoming studio! We are currently developing a bot and this site.
           </Text>
-        
-          
+          <Link href="/" style={{ textDecoration: 'none' }}>
+            <Button colorScheme="blue">Go Back</Button>
+          </Link>
         </Box>
       </Center>
     </>
-  )
-}
+  );
+};
 
 export default ContentPage
