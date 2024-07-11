4a93fa7ce8080aa410ff19916c2d100dcdf6683d
Noblixzy <121813330+adityaREDFLAG@users.noreply.github.com>
2024-07-11 09:37:25 +0530

    Update info.tsx

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,14 +1,14 @@
-import * as React from 'react';
-import { NextSeo } from 'next-seo';
-import { Center, Box, Heading, Text, Button } from '@chakra-ui/react';
+import * as React from 'react'
+import { NextSeo } from 'next-seo'
+import { Center, Box, Heading, Text } from '@chakra-ui/react'
 
-import type { NextPage } from 'next';
+import type { NextPage } from 'next'
 
 const ContentPage: NextPage = () => {
   return (
     <>
       <NextSeo title="Info" titleTemplate="%s"/>
-      <Center minH="100vh" position="relative">
+      <Center minH="100vh">
         <Box textAlign="center" p={4}>
           <Heading as="h1" size="xl" mb={4}>
             What's Adi's Studio
@@ -16,21 +16,24 @@ const ContentPage: NextPage = () => {
           <Text fontSize="lg">
             Adi's studio is an upcoming studio! We are currently developing a bot and this site.
           </Text>
-        </Box>
-        <Box position="absolute" bottom={4} left={4}>
-          <Button
-            as="a"
-            bg="transparent"
-            border="1px solid"
-            borderColor={siteConfig.themeColor}
-            href="/"
-          >
-            Go Back
-          </Button>
+          
+        <Wrap justify="center" spacing={4}>
+          <WrapItem>
+            <Button
+              as="a"
+              bgColor={siteConfig.themeColor}
+              href="/"
+              
+            >
+              Check it out →
+            </Button>
+          </WrapItem>
+        </Wrap>
+          
         </Box>
       </Center>
     </>
-  );
+  )
 }
 
 export default ContentPage
