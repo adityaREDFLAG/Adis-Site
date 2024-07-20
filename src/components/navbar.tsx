344ff00346121675ff9b0fce43ae94c298dfd2ab
Noblixzy <121813330+adityaREDFLAG@users.noreply.github.com>
2024-07-20 17:27:42 +0530

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { HStack, Icon, IconButton, Link, useColorMode } from '@chakra-ui/react';
+import { HStack, Icon, IconButton, Link, useColorMode, VStack, Text } from '@chakra-ui/react';
 import { FaMoon } from 'react-icons/fa';
 import NextLink from 'next/link';
 import { useSocials } from '@/hooks/app';
@@ -16,20 +16,17 @@ export const Navbar: React.FC = () => {
       </Link>
 
       <HStack flexGrow={1} justify="flex-end" p={4} spacing={{ base: 0, sm: 2 }}>
-        {socials.map(([href, SocialIcon]) => (
-          <IconButton
-            as="a"
-            aria-label={href}
-            color="currentColor"
-            href={href}
-            icon={<Icon as={SocialIcon} boxSize={5} />}
-            key={href}
-            variant="link"
-          />
-        ))}
+        <VStack align="start">
+          <Text fontWeight="bold">Projects</Text>
+          <NextLink href="/to-do" passHref>
+            <Link>To-Do↗</Link>
+          </NextLink>
+        </VStack>
+        
         <Link href="https://status.uptime-monitor.io/669b90860cf6e5ca54b87cac?_gl=1*vyk1f*_gcl_au*MjE0NzczNTY0LjE3MjE0NzA4NzU.*_ga*MTg3MTAyMjQwMS4xNzIxNDcwODc1*_ga_Z8TT3FLVJQ*MTcyMTQ3MDg3NC4xLjEuMTcyMTQ3MTA5MS41OC4wLjA." isExternal>
           Uptime↗
         </Link>
+        
         <IconButton
           aria-label="toggle dark mode"
           color="currentColor"
